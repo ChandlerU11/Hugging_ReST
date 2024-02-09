583c430da4fd8431e3afa4f3c1b3bfcd333313b7
Chandler Underwood <91697810+ChandlerU11@users.noreply.github.com>
2024-02-09 10:15:17 -0700

    Update ReST_Method_Fine.py

--- ReST_Method_Fine.py
+++ ReST_Method_Fine.py
@@ -25,7 +25,7 @@ def dummy_reward_model(df):
 
     # Count number of "hugs" in response
     hug_count = [sum(1 for i in each.split() if i == 'hugs') for each in df['response']]
-    
+
     # If count of "hugs" and total words in string do not match input legnth, give negative reward
     rewards = []
     for x,y,z in zip(input_len, hug_count, gen_len):
@@ -53,74 +53,77 @@ def generate(df, gen_model, tokenizer, generation_kwargs, N):
 def ReST(D, Deval, G, I, N, model, tokenizer, generation_kwargs, training_args):
     for g in range(G):
         print('Grow Step ', g)
-        train_df = D.copy()
 
-        train_df = generate(train_df, model, tokenizer, generation_kwargs, N)
-        train_df['scores'] = dummy_reward_model(train_df)
-        print(len(train_df[train_df['scores'] == 0]), "generations out of ", len(train_df), "are the correct length.")
+        # Generate Dg. N determines number of generations per sample.
+        Dg = generate(D, model, tokenizer, generation_kwargs, N)
+
+        # Annotate Dg with reward model.
+        Dg['scores'] = dummy_reward_model(Dg)
+
+        print(len(Dg[Dg['scores'] == 0]), "generations out of ", len(Dg), "are the correct length.")
         print("Example output from model:")
-        print(train_df.head(50))
+        print(Dg.head(25))
         time.sleep(10)
 
         steps = 0
         for tau_i in I:
             print('Improve Step: ', steps)
             print('Threshold: ', tau_i)
-            
+
             # Filter for samples at or above threshold
-            Dg = train_df.loc[(train_df['scores'] >= tau_i)].copy()
-            if len(Dg) == 0:
+            Dg_filt = Dg.loc[(Dg['scores'] >= tau_i)].copy()
+            if len(Dg_filt) == 0:
                 print("NO SAMPLES ABOVE THRESHOLD")
                 break
 
-            Dg = Dataset.from_pandas(Dg).map(prepare_dataset, batched=True)
+            Dg_filt = Dataset.from_pandas(Dg_filt).map(prepare_dataset, batched=True)
 
             # Create trainer with newly filtered data
-            trainer = Seq2SeqTrainer(model=model, args=training_args, tokenizer=tokenizer, train_dataset=Dg, data_collator=data_collator)
+            trainer = Seq2SeqTrainer(model=model, args=training_args, tokenizer=tokenizer, train_dataset=Dg_filt, data_collator=data_collator)
 
             # First fine-tuning of improve step
             trainer.train()
 
-            # Evaluation
-            Dg_e = Deval.copy()
-
             # Generate one response to for every sample in eval set
-            Dg_e = generate(Dg_e, model, tokenizer, generation_kwargs, 1)
-            Dg_e['scores'] = dummy_reward_model(Dg_e)
-            
-            prev = 0
-            improve = statistics.mean(Dg_e['scores'])
-            
-            # While model improves reward model score on eval set, continue to fine-tune with newly filtered data
+            Dg_eval = generate(Deval, model, tokenizer, generation_kwargs, 1)
+            Dg_eval['scores'] = dummy_reward_model(Dg_eval)
+
+            # While model improves reward model score on eval set, continue to fine-tune using Dg_filt
+            prev = -5
+            improve = statistics.mean(Dg_eval['scores'])
             while prev < improve:
                 trainer.train()
-                Dg_e = Deval.copy()
-                Dg_e = generate(Dg_e, model, tokenizer, generation_kwargs, 1)
-                Dg_e['scores'] = dummy_reward_model(Dg_e)
+                Dg_eval = generate(Deval, model, tokenizer, generation_kwargs, 1)
+                Dg_eval['scores'] = dummy_reward_model(Dg_eval)
                 prev = improve
-                improve = statistics.mean(Dg_e['scores'])
-            
+                improve = statistics.mean(Dg_eval['scores'])
+
             steps += 1
-    
+
     print("Training Finished!!!")
     return model
 
 def test_ReST(test_data, model, tokenizer, generation_kwargs):
       test_df = generate(test_data, model, tokenizer, generation_kwargs, 1)
       test_df['scores'] = dummy_reward_model(test_df)
-      print("The fine-tuned model generates the correct number of hugs", len(test_df[test_df['scores'] == 0]) / len(test_df) * 100, "percent of the time!" )
+      print("The fine-tuned model now generates the correct number of hugs", len(test_df[test_df['scores'] == 0]) / len(test_df) * 100, "percent of the time!" )
+      print(test_df.head(25))
 
-
-rand_list = []
+# Generate training data
+rand_data = []
 for i in range(1000):
-     rand_list.append(str(random.randrange(1,5)) + ' hugs')
+     rand_data.append(str(random.randrange(1,5)) + ' hugs')
 
-test_data = []
+# Generate test data
+rand_test_data = []
 for i in range(100):
-     test_data.append(str(random.randrange(1,5)) + ' hugs')
+     rand_test_data.append(str(random.randrange(1,5)) + ' hugs')
+
+train_df = pd.DataFrame()
+train_df['input'] = rand_data
 
 test_df = pd.DataFrame()
-test_df['input'] = test_data
+test_df['input'] = rand_test_data
 
 generation_kwargs = {
     "min_length":-1,
@@ -138,18 +141,15 @@ training_args = Seq2SeqTrainingArguments(
             per_device_train_batch_size = 64
             )
 
-
 tokenizer = AutoTokenizer.from_pretrained("google-t5/t5-small")
 model = AutoModelForSeq2SeqLM.from_pretrained("google-t5/t5-small").to(device)
 data_collator = DataCollatorForSeq2Seq(tokenizer=tokenizer, label_pad_token_id=-100)
 
-train_df = pd.DataFrame()
-train_df['input'] = rand_list
 D, Deval = train_test_split(train_df, test_size = .1, random_state = 42)
-
-G = 3
-I = [-2,-1,0]
-N = 10
+G = 3          # Number of Grow Steps
+I = [-2,-1,0]  # Number of Improve Steps (length of list) with respective thresholds
+N = 10         # Number of generations for each sample when creating Dg
 
 fine_tuned_model = ReST(D, Deval, G, I, N, model, tokenizer, generation_kwargs, training_args)
+
 test_ReST(test_df, fine_tuned_model, tokenizer, generation_kwargs)
